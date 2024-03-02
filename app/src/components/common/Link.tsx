252ed3cdcb30834a781b3ba0572c6dd1923352d6
Simon Maurer <maurer.simon@live.de>
2024-03-02 21:29:41 +0100

    some work on Link

--- app/src/components/common/Link.tsx
+++ app/src/components/common/Link.tsx
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { FC, ReactNode, MouseEvent } from 'react';
+
+// Links can have 1 of 4 types: internal, external, download, or anchor
+// Their hrefs look like this:
+// internal: "Entry", "Subfolder/Entry"
+// external: "https://example.com" or also "https://thissite.com", if the page is not part of the knowledge base
+// download: "/file.xlsx", "/file.pdf"
+// anchor: "Entry#Heading With Spaces"
+enum LinkType {
+    INTERNAL = 'internal',
+    EXTERNAL = 'external',
+    DOWNLOAD = 'download',
+    ANCHOR = 'anchor',
+}
 
 interface LinkProps {
     href: string;
-    target?: string;
-    rel?: string;
     className?: string;
-    children: React.ReactNode;
+    linkText?: ReactNode;
+    linkType?: LinkType;
+    onClick?: () => void;
+    onMiddleClick?: () => void;
 }
 
-const Link: React.FC<LinkProps> = ({ href, target, rel, className, children }) => {
+const Link: FC<LinkProps> = ({ href, className, linkText, linkType, onClick, onMiddleClick }: LinkProps) => {
+
+    const handleMouseClick = (event: MouseEvent) => {
+        event.preventDefault();
+        if (event.button === 1 && onMiddleClick) onMiddleClick();
+        else if (onClick) onClick();
+    }
+
     return (
-        <a href={href} target={target} rel={rel} className={className}>
-            {children}
+        <a href={href} className={className} onClick={handleMouseClick}>
+            {linkText || href}
         </a>
     );
 };
