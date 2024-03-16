965d59fcb29cecd8caec322988bf026e5579d0f4
Simon Maurer <maurer.simon@live.de>
2024-03-16 15:47:57 +0100

    cleanup types

--- app/src/scripts/types.ts
+++ app/src/scripts/types.ts
@@ -1,19 +1,13 @@
+// types for the index.json file
 interface Index {
     vault: string;
     attachments: string;
     homepage: string;
     index: (IndexFolder | IndexEntry)[];
 }
-
 interface IndexFolder {
     contents?: (IndexFolder | IndexEntry)[]
 }
-
-enum EntryAction {
-    OPEN = 'open',
-    CLOSE = 'close'
-}
-
 interface IndexEntry {
     title: string;
     created_time: number;
@@ -22,9 +16,15 @@ interface IndexEntry {
     aliases?: string[];
     tags?: string[];
 }
+export type { Index, IndexFolder, IndexEntry };
+
+enum EntryAction {
+    OPEN = 'open',
+    CLOSE = 'close'
+}
 
 type OpenEntry = (entry: string, middleMouse?: boolean) => void;
 
-export type { Index, EntryAction, IndexFolder, IndexEntry, OpenEntry };
+export type { EntryAction, OpenEntry };
 
 
