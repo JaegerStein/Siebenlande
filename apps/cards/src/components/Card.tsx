64c30e4c13687cbf5c5504057f156f8f163d10e7
Simon Maurer <maurer.simon@live.de>
2025-09-27 19:53:24 +0200

    rescaler

--- apps/cards/src/components/Card.tsx
+++ apps/cards/src/components/Card.tsx
@@ -1,26 +1,50 @@
-import type { ReactElement } from 'react';
-import { useState, useEffect } from 'react';
-
+import { useState, useEffect, useRef } from 'react';
 import { ShieldHalf, Swords, Skull, HandFist, Flame, Gem, Milk } from 'lucide-react';
-
 import './Card.scss';
 
-function getScale(): number {
-	const [w, h] = [window.innerWidth, window.innerHeight];
+const baseWidth = 1024;
+const baseHeight = 1536;
+
+function getScale(parent: HTMLElement): number {
+	const cs = getComputedStyle(parent);
+	const [w, h] = [
+		parent.clientWidth - parseFloat(cs.paddingLeft) - parseFloat(cs.paddingRight),
+		parent.clientHeight - parseFloat(cs.paddingTop) - parseFloat(cs.paddingBottom)
+	];
 	if (w === 0 || h === 0) return 1;
-	return Math.min(w / 1024, h / 1536, 1);
+	return Math.min(w / baseWidth, h / baseHeight, 1);
 }
 
-function Card(): ReactElement {
-	const [scale, setScale] = useState(getScale());
+const Card: React.FC = () => {
+	const [scale, setScale] = useState(1);
+	const ref = useRef<HTMLDivElement | null>(null);
+	const timeout = useRef<number| null>(null);
+
 	useEffect(() => {
-		const onResize = () => setScale(getScale());
-		window.addEventListener('resize', onResize);
-		return () => window.removeEventListener('resize', onResize);
+		const parent = ref.current?.parentElement;
+		if (!parent) return;
+		setScale(getScale(parent));
+
+		const observer = new ResizeObserver(() => {
+			if (timeout.current) window.clearTimeout(timeout.current);
+			timeout.current = window.setTimeout(() => {
+				timeout.current = null;
+				setScale(getScale(parent));
+			}, 100);
+		});
+
+		observer.observe(parent);
+
+		return () => {
+			observer.disconnect();
+			if (timeout.current) window.clearTimeout(timeout.current);
+		}
+
 	}, []);
 
 	return (
 		<div
+			ref={ref}
 			id='card'
 			style={{ transform: `scale(${scale})` }}
 		>
