46b52be05a9b8689234adaae041b829c365f4ea0
Simon Maurer <maurer.simon@live.de>
2025-09-27 16:53:37 +0200

    working iframe

---
+++ Siebenlande/Werkzeuge/Inventar.md
@@ -0,0 +1,5 @@
+---
+tags:
+  - Werkzeug
+---
+<iframe width="100%" height="768px" src="/Werkzeuge/cards.html">
--- apps/cards/vite.config.ts
+++ apps/cards/vite.config.ts
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "./",
+  build: {
+    outDir: '../../Siebenlande/Werkzeuge/',
+    rollupOptions: {
+      input: 'cards.html'
+    }
+  }
 })
